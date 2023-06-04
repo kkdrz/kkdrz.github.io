8b9f1e28823915365b54d29ff98876f356629f07
Konrad Drozd <konrad.drozd@ocado.com>
2023-06-04 12:43:17 +0200

    Buy me a coffee

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import SocialSquare from "./SocialSquare"
-import { graphql, useStaticQuery } from "gatsby"
+import {graphql, useStaticQuery} from "gatsby"
 
 const Footer = () => {
   const { site } = useStaticQuery(
@@ -25,20 +25,27 @@ const Footer = () => {
       style={{ backgroundColor: "#323232" }}
     >
       <div>
-        <div className="md:w-128 m-auto py-8 px-4 flex flex-col">
+        <div className="md:w-128 w-full m-auto py-8 px-4 flex flex-col">
           <div className="uppercase pb-2 font-bold text-sm text-gray-100 mb-2">
             You can find me there:
           </div>
-          <div>
-            {site.siteMetadata.social?.map((social, i) => (
-              <SocialSquare
-                key={i}
-                className="text-gray-100 p-2 hover:text-blue-100"
-                title={social.title}
-                href={social.url}
-                icon={social.icon}
-              ></SocialSquare>
-            ))}
+          <div className="flex flex-col md:flex-row">
+            <div className="grow mx-auto">
+              {site.siteMetadata.social?.map((social, i) => (
+                  <SocialSquare
+                      key={i}
+                      className="text-gray-100 p-2 hover:text-blue-100"
+                      title={social.title}
+                      href={social.url}
+                      icon={social.icon}
+                  ></SocialSquare>
+              ))}
+            </div>
+            <div className="mx-auto mt-2 md:mt-0">
+              <a href="https://www.buymeacoffee.com/kdrozd"><img
+                  src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=kdrozd&button_colour=f3f4f6&font_colour=000000&font_family=Lato&outline_colour=000000&coffee_colour=FFDD00"
+                  alt={"Buy me a coffee"}/></a>
+            </div>
           </div>
         </div>
       </div>
